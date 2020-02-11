85c9b499891f320803d71e30534164f44d94dd25
max-student <max-student@maxtrain.com>
2020-02-11 14:41:33 -0500

    Add project files.

---
+++ NullableTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NullableTypes {
+    class Program {
+        static void Main(string[] args) {
+
+            var students = new Students[] {
+            new Students(1, "Kelly Hudson", null, "3.75"),
+            new Students(2, "John Shelton", 1200, "3.9"),
+            new Students(3, "Blake Legend", 900, "3.5"),
+            new Students(4, "Gwen Clarkson", 1000, "3.68"),
+            };
+
+            foreach (var student in students) {
+                 student.Print();
+            }
+        }
+    }
+}
---
+++ NullableTypes/Students.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NullableTypes {
+    class Students {
+        public int ID;
+        public string Name;
+        public int? SAT;
+        public string GPA;
+
+
+
+        public Students(int id, string name, int? sat, string gpa) {
+            this.ID = id;
+            this.Name = name;
+            this.SAT = sat;
+            this.GPA = gpa;
+    
+        }
+
+        public void Print() {
+            Console.WriteLine($"{this.ID}. {this.Name} has an SAT score of {this.SAT} and a GPA of {this.GPA}.");
+        }
+
+    }
+
+
+}
